009bc140b6c009ff4d9a598dfc7ab4884d752143
Christopher Holder <toholderandrews@gmail.com>
2021-11-28 08:04:07 -0600

    feat: add dependencies and necesary configs to run

--- run.py
+++ run.py
@@ -1,3 +1,30 @@
+import os
+import asyncio
+from datetime import datetime
+import random
+from uuid import uuid4
+import logging
+
+from pyrogram import Client, filters
+from pyrogram.errors import MessageNotModified, FloodWait
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup,\
+CallbackQuery, ReplyKeyboardMarkup
+
+from callback_messages import HELP_TEXT
+
+#logging.basicConfig(filename='run.log', level=logging.DEBUG,
+#                    format='%(asctime)s:%(levelname)s:%(message)s')
+
+app_name = 'DeepBlue_Telegram_Bot'
+api_id = int(os.environ['API_ID'])
+api_hash = os.environ['API_HASH']
+bot_token = os.environ['BOT_TOKEN']
+
+app = Client(
+    app_name, api_id, api_hash, bot_token
+)
+
+
 ## global varibles
     ## countdowns
         ## countdown_id
@@ -48,3 +75,6 @@
 
 ## Command 'kill'
     # only super user
+
+print("Countdown is alive!")
+app.run()
