c5f5dbaef7d7ea1313f50146d6b515c380313b16
Christopher Holder <toholderandrews@gmail.com>
2021-11-27 20:57:38 -0600

    feat: add new file with  sudo code and structure of desired outcome

---
+++ run.py
@@ -0,0 +1,49 @@
+## global varibles
+    ## countdowns
+        ## countdown_id
+        ## owner_username
+        ## owner_id
+        ## name
+        ## date
+        ## inicial message
+        ## event link
+        ## photo
+        ## caption
+
+## command '/help 'command'(optional) 'verbose'(optional)
+    ## if no args then list commands and small descriptions
+    ## if 1 arg and arg is in list of commads
+        # in depth description of command
+        # if -v as arg 2 add examples
+
+## Command /set 'id'
+    # only chat admin
+        # if date in future
+            # create countdown message
+            # pin created message
+            # wait 5 to 8 seconds
+            # if date in future
+                # update message
+                # edit pinned message
+            # else if date in passed
+                # unpin countdown message
+                # create launch message
+                # send launch message
+                # pin launch message
+        # else if date not in past
+            # send private error message
+    # if not admin, log event
+
+## Command /create name
+    # Only in admin chat
+
+## Command /edit id field data
+    # Only countdown owners
+
+## Command /preview id
+
+## Command /stop
+    # only countdown owners
+
+## Command 'kill'
+    # only super user
