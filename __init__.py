6b7d5f6c419a7b9e596a5a9f5852cdd43e773d8e
Christopher Holder <toholderandrews@gmail.com>
2022-01-17 14:14:00 -0600

    feat: iniciates a table to maintain running countdowns

--- __init__.py
+++ __init__.py
@@ -13,9 +13,20 @@ def iniciate_countdowns_table(cur):
         countdown_state text
     )""")
 
+def initciate_running_countdown_table(cur):
+    cur.execute("""CREATE TABLE running_countdowns (
+        countdown_id text, 
+        countdown_owner_id text,
+        message_chat_id text,
+        message_chat_title text,
+        message_id text,
+        countdown_state text
+    )""")
 
 if __name__ == '__main__':
     conn = sqlite3.connect('database.db')
     cur = conn.cursor()
     iniciate_countdowns_table(cur)
+    initciate_running_countdown_table(cur)
+    conn.commit()
     conn.close()
