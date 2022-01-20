51dafe6f1086df557db59e2c695448ab133e4fee
Christopher Holder <toholderandrews@gmail.com>
2022-01-20 12:03:31 -0600

    fix: incorrect permissions message

--- permissions.py
+++ permissions.py
@@ -4,12 +4,12 @@ async def in_admin_group(message):
     admin_group_id = -783004423
     current_group_id = message.chat.id
     if admin_group_id != current_group_id:
-        await message.reply(ERR_P_1)
+        await message.reply(ERR_P_2)
     else: return True
 
 async def is_super_user(message):
     super_user_id = 964072920
     current_user_id = message.from_user.id
     if super_user_id != current_user_id:
-        await message.reply(ERR_P_2)
+        await message.reply(ERR_P_1)
     else: return True
